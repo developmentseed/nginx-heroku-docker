b3ea2372f0d965dda811644a07923c782c84e5af
Scisco <scisco7@gmail.com>
2015-05-21 11:54:03 -0400

    first commit

---
+++ readme.md
@@ -0,0 +1,35 @@
+# NGINX Binary for Docker in Heroku
+
+This docker app compiles a NGINX binary ready for use and deployment as a Docker app on Heroku. The binary is compiled against Heroku's Cedar-14 stack.
+
+If you want to run NGINX proxy on Heroku's managed environment, this is app is what you need.
+
+In order to use this app, you must have Heroku plugin for docker and docker installed on your computer. You can follow [this guide](https://devcenter.heroku.com/articles/introduction-local-development-with-docker?preview=1) to have the requirements installed.
+
+## Prepare the binary
+
+Run this command:
+
+    $ heroku docker:exec ./scripts/build_nginx.sh
+
+After the command is executed, nginx's binary is copied to `heroku/bin` folder.
+
+## Use and Deployment of NGINX
+
+After executing the previous step, simply copy the heroku folder to a new location and use it for launching and deploying a heroku app.
+
+To deploy a new heroku app using the content of `heroku` folder:
+
+```bash
+$ heroku create
+$ heroku docker:release
+$ heroku logs -t
+```
+
+## NGINX Config
+
+Edit content of `heroku/config/nginx.conf.erb`.
+
+## Credit
+
+- [nginx-buildpack](https://github.com/ryandotsmith/nginx-buildpack)
---
+++ scripts/build_nginx.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# Build NGINX and modules on Heroku.
+# This program is designed to run in a web dyno provided by Heroku.
+# We would like to build an NGINX binary for the builpack on the
+# exact machine in which the binary will run.
+# Our motivation for running in a web dyno is that we need a way to
+# download the binary once it is built so we can vendor it in the buildpack.
+#
+# Once the dyno has is 'up' you can open your browser and navigate
+# this dyno's directory structure to download the nginx binary.
+
+NGINX_VERSION=${NGINX_VERSION-1.5.7}
+PCRE_VERSION=${PCRE_VERSION-8.21}
+HEADERS_MORE_VERSION=${HEADERS_MORE_VERSION-0.23}
+
+nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
+pcre_tarball_url=http://garr.dl.sourceforge.net/project/pcre/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.bz2
+headers_more_nginx_module_url=https://github.com/agentzh/headers-more-nginx-module/archive/v${HEADERS_MORE_VERSION}.tar.gz
+
+temp_dir=$(mktemp -d /tmp/nginx.XXXXXXXXXX)
+
+echo "Serving files from /tmp on $PORT"
+cd /tmp
+python -m SimpleHTTPServer $PORT &
+
+cd $temp_dir
+echo "Temp dir: $temp_dir"
+
+echo "Downloading $nginx_tarball_url"
+curl -L $nginx_tarball_url | tar xzv
+
+echo "Downloading $pcre_tarball_url"
+(cd nginx-${NGINX_VERSION} && curl -L $pcre_tarball_url | tar xvj )
+
+echo "Downloading $headers_more_nginx_module_url"
+(cd nginx-${NGINX_VERSION} && curl -L $headers_more_nginx_module_url | tar xvz )
+
+(
+	cd nginx-${NGINX_VERSION}
+	./configure \
+		--with-pcre=pcre-${PCRE_VERSION} \
+		--prefix=/tmp/nginx \
+        --with-http_ssl_module \
+        --with-http_stub_status_module
+		--add-module=/${temp_dir}/nginx-${NGINX_VERSION}/headers-more-nginx-module-${HEADERS_MORE_VERSION}
+	make install
+)
+
+echo "Copying NGINX binary to bin folder"
+cp /tmp/nginx/sbin/nginx /app/src/heroku/bin/nginx-cedar-14
+
+echo "Copy mimetype"
+cp /tmp/nginx/conf/mime.types /app/src/heroku/config/mime.types
