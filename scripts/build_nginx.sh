f9a9780b2c2bcfa4f47ccda2d5b3d7ff5fcb5392
Scisco <scisco7@gmail.com>
2016-04-28 16:09:03 -0400

    use latest version of nginx and include stream ssl module

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -9,12 +9,12 @@
 # Once the dyno has is 'up' you can open your browser and navigate
 # this dyno's directory structure to download the nginx binary.
 
-NGINX_VERSION=${NGINX_VERSION-1.5.7}
-PCRE_VERSION=${PCRE_VERSION-8.21}
-HEADERS_MORE_VERSION=${HEADERS_MORE_VERSION-0.23}
+NGINX_VERSION=${NGINX_VERSION-1.9.7}
+PCRE_VERSION=${PCRE_VERSION-8.38}
+HEADERS_MORE_VERSION=${HEADERS_MORE_VERSION-0.29}
 
 nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
-pcre_tarball_url=http://garr.dl.sourceforge.net/project/pcre/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.bz2
+pcre_tarball_url=http://downloads.sourceforge.net/project/pcre/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.bz2
 headers_more_nginx_module_url=https://github.com/agentzh/headers-more-nginx-module/archive/v${HEADERS_MORE_VERSION}.tar.gz
 
 temp_dir=$(mktemp -d /tmp/nginx.XXXXXXXXXX)
@@ -41,7 +41,8 @@ echo "Downloading $headers_more_nginx_module_url"
 		--with-pcre=pcre-${PCRE_VERSION} \
 		--prefix=/tmp/nginx \
         --with-http_ssl_module \
-        --with-http_stub_status_module
+        --with-http_stub_status_module \
+        --with-stream_ssl_module \
 		--add-module=/${temp_dir}/nginx-${NGINX_VERSION}/headers-more-nginx-module-${HEADERS_MORE_VERSION}
 	make install
 )
